e550f334ac44cbc2c3baab0ee9b1998886df98b9
master762 <m8069248@gmail.com>
2025-06-02 00:40:01 +0300

    final

--- src/app/components/popularitems/popularitems.component.ts
+++ src/app/components/popularitems/popularitems.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-popularitems',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './popularitems.component.html',
   styleUrl: './popularitems.component.scss'
 })
